f054b724f675516ff16bd940b3777a7af5ec8a43
Aditya Mulgundkar <adityamulgundkar.nmims@gmail.com>
2021-04-13 13:02:27 +0530

    Initial commit
    Started the dummy repository & hosted generated .nupkg at https://www.nuget.org/packages/opencv-dotnet-test/

---
+++ main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace opencv_dotnet
+{
+    public class Logger
+    {
+        public void Log(string text){
+            // Logger class for testing purposes
+            Console.WriteLine(text);
+        }
+    }
+}
